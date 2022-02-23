8a210a14fc2b558b50c7df8e4259b263b74bf8bb
iamLolade <adeniyiotemade@gmail.com>
2022-02-23 01:56:22 +0100

    Updated About component, utilized useNavigate feature

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,18 @@
-import { Route } from "react-router-dom";
+import { Route, useNavigate } from "react-router-dom";
 
 //Nested route
 import Offers from "./Offers"
 
 const About = () => {
+    const navigate = useNavigate();
+
     return ( 
         <div className="content">
             <h2>About Us</h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia voluptatibus perspiciatis aut laboriosam ipsum, repellat corporis rem officia ex tenetur accusamus, expedita doloribus molestias at qui iste, nostrum animi est! Atque nihil placeat ea libero quas quia minima ut totam? Consequuntur nam dolor nesciunt accusantium, maiores in vitae nostrum accusamus.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi pariatur itaque eius placeat a quo distinctio. Vitae placeat expedita sunt ab, accusamus, at iusto dolorum, suscipit nemo deserunt blanditiis praesentium?</p>
 
+            <button onClick={() => navigate("/products")}>See products</button>
             {/* <Route path="/about/offers" component={Offers}/> */}
         </div>
      );
